b8dbd7542696e48380200a79cf64b9cea28fee18
Hermann-S <126822504+Hermann-S@users.noreply.github.com>
2023-04-25 15:20:00 -0600

    struggled with strike zone and started the pitching

--- sketch.js
+++ sketch.js
@@ -16,8 +16,13 @@ function draw() {
   rect(mouseX, mouseY, 70, 30);
   fill(255, 255, 255, 50);
   stirkeZone();
+  pitcher();
 }
 
 function stirkeZone() {
   rect(width/2, height*0.65, 200, 300);
+}
+
+function pitcher() {
+  circle(width/2, height/2, 30);
 }
