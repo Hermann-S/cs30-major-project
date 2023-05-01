2c32b122349fef2b6955fcf04227f63c3c650ded
Hermann-S <126822504+Hermann-S@users.noreply.github.com>
2023-05-01 15:20:49 -0600

    pitcher function working for the most part
    installed timer library and used it for the function... PRAISE MR. SCHELLENBERG

--- sketch.js
+++ sketch.js
@@ -5,14 +5,18 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let ballSize;
+let ballSize = 15;
+let ballZoomTimer;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // eslint-disable-next-line no-undef
+  ballZoomTimer = new Timer(100);
+  ballZoomTimer.start();
 }
 
 function draw() {
-  background(220);
+  background("salmon");
   rectMode(CENTER);
   rect(mouseX, mouseY, 70, 30);
   fill(255, 255, 255, 50);
@@ -27,19 +31,30 @@ function stirkeZone() {
 
 function pitcher() {
   
-  // the ball is not inceasing at a visible rate
-  for (let m = millis(); m < 5000; m++) {
-    // if (m > 5000) {
-      ballSize = 15;
-      for (let i = 15;i < 30; i++) {
-        ballSize += 1;
-        circle(width/2, height*0.65, ballSize);
-        millis(2000);
-      }
-    // }
+  if (ballZoomTimer.expired()) {
+     (ballSize === 15) {
+      ballZoomTimer.start();
+    }
+  else {
+      ballSize += 1;
+    }
   }
-  console.log();
 }
+
+  circle(width/2, height*0.65, ballSize);
+
+  // the ball is not inceasing at a visible rate
+  // everything hurts
+  // ballSize = 15;
+  
+  // for (let m = millis(); m < 5000; m++) {
+  //   for (let i = 15;i < 30; i++) {
+  // ballSize += 1;
+  // circle(width/2, height*0.65, ballSize);
+  //     millis(2000);
+  //   }
+  // }
+  // console.log();
 
 // function batting() {
 
@@ -49,4 +64,3 @@ function pitcher() {
 //     pitcher.y += 5;
 //   }
 // }
-
