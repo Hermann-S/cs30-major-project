df872751d29e09f44e88d2c040949b075baa5ad8
Hermann-S <126822504+Hermann-S@users.noreply.github.com>
2023-05-03 15:20:13 -0600

    finished basic pitch working on bat

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state = "moving";
 let ballSize = 5;
 let ballZoomTimer;
 let x;
@@ -19,16 +20,18 @@ function setup() {
   // eslint-disable-next-line no-undef
   ballZoomTimer = new Timer(100);
   ballZoomTimer.start();
+  // eslint-disable-next-line no-undef
+  ballStopTimer = new Timer(1500);
 }
 
 function draw() {
-  background("orange");
+  background(138, 8, 8);
   rectMode(CENTER);
   rect(mouseX, mouseY, 70, 30);
   fill(255, 255, 255, 50);
   stirkeZone();
   pitcher();
-  // batting();
+  batting();
 }
 
 function stirkeZone() {
@@ -39,43 +42,37 @@ function pitcher() {
   x = width/2;
   y = height*0.65;
   // eslint-disable-next-line no-undef
-  ballStopTimer = new Timer(500);
   if (ballZoomTimer.expired()) {
     circle(x, y, ballSize);
-    if (ballSize === 30) {
-      // need to remove circle
+    ballStopTimer.start();
+    if (ballSize >= 30) {
       // this circle is going to remove me...
-      ballStopTimer.start();
-      if (ballStopTimer.expired()) {
-        isBallShowing = false;
-      }
-
+      ballZoomTimer.pause();
+      ballZoomTimer.reset();
     }
     else {
-      ballSize += 0.5;
+      ballSize += 0.3;
     }
   }
 }
 
 
-// the ball is not inceasing at a visible rate
 // everything hurts
-// ballSize = 15;
-
-// for (let m = millis(); m < 5000; m++) {
-//   for (let i = 15;i < 30; i++) {
-// ballSize += 1;
-// circle(width/2, height*0.65, ballSize);
-//     millis(2000);
-//   }
-// }
-// console.log();
-
-// function batting() {
-
-//   // bat is not having an effect on the ball
-//   let d = dist(pitcher.x, pitcher.y, mouseX, mouseY);
-//   if (d < 15) {
-//     pitcher.y += 5;
-//   }
-// }
+// bat is not having an effect on the ball
+function batting() {
+  let d = dist(x, y, mouseX, mouseY,d < 30);
+  if (ballSize >= 20) {
+    state = "hit";
+  }
+  if (state === "hit") {
+    if (d <= 30) {
+      y += 5;
+    }
+  }
+  // if (d < 15) {
+  //   y += 5;
+  // }
+  else {
+    state = "moving";
+  }
+}
